b5a8377e082ca1d5621458d4c6a7ea09946de79c
Kerbidiah <28950544+Kerbidiah@users.noreply.github.com>
2021-09-27 22:58:46 -0400

    commit and sync
    by me

--- src/controls.rs
+++ src/controls.rs
@@ -1,7 +1,7 @@
 use macroquad::prelude::*;
 
 const MOVE_SPEED: f32 = 5.0; // m/s
-const LOOK_SPEED: f32 = 30.0;
+const LOOK_SPEED: f32 = 50.0;
 const GLOBAL_UP: Vec3 = Vec3::Y; // jHat
 
 #[derive(Debug)]
