6d02a59618f4a91296af36526df83492b6afc4d0
Kerbidiah <28950544+Kerbidiah@users.noreply.github.com>
2021-09-28 01:13:37 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ read 6D59713374367739
 cp target/wasm32-unknown-unknown/release/balistics.wasm pages/balistics.wasm
 
 # git stuff
-git add --all
 gh repo sync
+git add --all
 git commit -F commit.msg
 gh repo sync --force
